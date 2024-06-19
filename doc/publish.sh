2901514247a30b9e1be2a9b68aadc0ab14a1fac1
sideprojectslab <sideprojectslab@gmail.com>
2024-06-19 23:11:34 +0200

    1

--- doc/publish.sh
+++ doc/publish.sh
@@ -1,6 +1,6 @@
 asciidoctor-pdf -r asciidoctor-mathematical -a pdf-theme=resources/theme.yml -a env-pdf README.adoc -o ../README.pdf
 rm -rf stem*
-asciidoctor README.adoc ../README.html
+asciidoctor README.adoc -o ../README.html
 #asciidoctor -b docbook README.adoc
 #pandoc -f docbook -t markdown_strict README.xml -o ../README.md
 #rm -rf README.xml
