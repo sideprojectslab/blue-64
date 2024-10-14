3901511309f9f595b5c325cbfa662a97ef148646
sideprojectslab <sideprojectslab@gmail.com>
2024-10-14 21:24:35 +0200

    updated readme

--- doc/publish.sh
+++ doc/publish.sh
@@ -1,4 +1,4 @@
 #asciidoctor-pdf -r asciidoctor-mathematical -a pdf-theme=resources/theme.yml -a env-pdf asciidoc/README.adoc -o README.pdf
-asciidoctor-pdf -r asciidoctor-mathematical -a env-pdf asciidoc/README.adoc -o README.pdf
+asciidoctor-pdf -a env-pdf asciidoc/README.adoc -o README.pdf
 rm -rf stem*
 #asciidoctor -a env-web asciidoc/README.adoc -o README.html
